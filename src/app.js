65fe03df99b5cd0ec85bf38aae11fbd0df160e26
toshadvidrine <159669056+toshadvidrine@users.noreply.github.com>
2024-04-25 18:31:20 +0000

    new codespace

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "my"];
+  let adj = ["hairy", "spiked"];
+  let noun = ["creature", "monster"];
+
+  console.log(pronoun);
+
+  for (let i = 0; i < pronoun.length; i++) {
+    console.log(pronoun[i]);
+  }
+
+  for (let j = 0; j < adj.length; j++) {
+    console.log(adj[j]);
+  }
+  for (let k = 0; k < noun.length; k++) {
+    console.log(noun[k]);
+  }
 };
+console.log(` index i : ${i} \n index j : ${j} \n index k : ${k}`);
+
